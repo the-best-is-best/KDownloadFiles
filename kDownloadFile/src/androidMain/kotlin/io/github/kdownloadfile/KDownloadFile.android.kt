3aa66d9789d365d7c4c2f368afd25f7fdbcd54b5
Michelle Raouf <michelle.raouf@outlook.com>
2025-06-18 23:42:44 +0300

    Add User-Agent header to Android download requests
    This commit modifies the Android implementation of `downloadFile` to include a "User-Agent" header in the download request. This helps to mimic browser behavior and may resolve issues with servers that require a User-Agent for downloads.

--- kDownloadFile/src/androidMain/kotlin/io/github/kdownloadfile/KDownloadFile.android.kt
+++ kDownloadFile/src/androidMain/kotlin/io/github/kdownloadfile/KDownloadFile.android.kt
@@ -77,7 +77,7 @@ actual suspend fun downloadFile(
     folderName: String?,
 ): Result<String> = withContext(Dispatchers.IO) {
     suspendCancellableCoroutine { continuation ->
-        val context = AndroidKDownloadFile.appContext // احصل على applicationContext وليس activity
+        val context = AndroidKDownloadFile.appContext
         val destinationPath = if (!folderName.isNullOrEmpty()) {
             "$folderName/$fileName"
         } else {
@@ -91,6 +91,10 @@ actual suspend fun downloadFile(
                 .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, destinationPath)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
+                .addRequestHeader(
+                    "User-Agent",
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+                )
 
             val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
             val downloadId = downloadManager.enqueue(request)
